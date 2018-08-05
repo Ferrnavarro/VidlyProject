2b297c0bdbb2e4c102d3218ec28c43c8d6d5138d
Ferrnavarro <fer.maganavarro@gmail.com>
2018-08-04 21:42:47 -0600

    Exercise section 6

---
+++ Vidly/Dtos/MovieDto.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Vidly.Dtos
+{
+    public class MovieDto
+    {
+
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+      
+        public DateTime ReleaseDate { get; set; }       
+      
+        [Range(1, 25)]
+        public short NumberInStock { get; set; }
+      
+        public byte GenreId { get; set; }
+
+        public DateTime DateAdded { get; set; }
+
+
+
+
+    }
+}
